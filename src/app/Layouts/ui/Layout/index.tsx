869014db49efecb2eea8d1807299e9693a35fd62
XGM <109265751+xgmxgm@users.noreply.github.com>
2023-09-03 20:44:03 +0600

    year
    year

--- src/app/Layouts/ui/Layout/index.tsx
+++ src/app/Layouts/ui/Layout/index.tsx
@@ -1,12 +1,10 @@
 import { FC, type PropsWithChildren } from 'react'
 
 import styles from './Layout.module.scss'
-import { Header } from '@/widgets/Header'
 
 export const Layout: FC<PropsWithChildren> = ({children}) => {
     return (
         <div className={ styles.Layout }>
-            {/* <Header /> */}
             {children}
         </div>
     )
